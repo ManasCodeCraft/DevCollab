6bd6f71fbfdca7d61a2b492b92a124850ff76f68
ManasCodeCraft <srivastavamanas941@gmail.com>
2024-06-16 19:17:57 +0530

    commit

--- src/components/Auth/loginForm.js
+++ src/components/Auth/loginForm.js
@@ -3,7 +3,7 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import "../../styles/login.css";
 import { UserAuthenication } from "../../redux/api/authAPI";
 import { message } from "../../globalComponents/utilityModal";
@@ -18,6 +18,7 @@ const validationSchema = z.object({
 });
 
 export default function LoginForm() {
+  const baseURL = useSelector(state=>state.config.baseURL);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -106,7 +107,7 @@ export default function LoginForm() {
         <Button variant="outline-info" onClick={() => navigate("/auth/signup")}>
           No Account? Create One
         </Button>
-        <Button variant="light" href="/auth/google" className="googleSignInBtn">
+        <Button variant="light" href={`${baseURL}/auth/google`} className="googleSignInBtn">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="30px" height="30px">
             <path fill="#fbc02d" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12	s5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24s8.955,20,20,20	s20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"/>
             <path fill="#e53935" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039	l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"/>
